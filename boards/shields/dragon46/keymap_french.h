afb95bab82b630cdbf0a0de344f6462e922023c8
bertrand <bertrand.fournier.pro@gmail.com>
2024-08-31 16:52:38 +0200

    agrv et start (windows)

--- boards/shields/dragon46/keymap_french.h
+++ boards/shields/dragon46/keymap_french.h
@@ -75,7 +75,7 @@
 #define FR_L    L    // L
 #define FR_M    SEMI // M
 #define FR_UGRV SQT  // ù
-#define FR_ASTR STAR // *
+#define FR_ASTR NUHS // *
 // Row 4
 #define FR_LABK NUBS // <
 #define FR_W    Z    // W
